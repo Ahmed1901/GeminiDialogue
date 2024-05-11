774dc05dbe6c4b1351982c94d93689b27ca78192
Muntasir Billah Munna <muntasir@exabyting.com>
2024-05-11 08:07:03 +0600

    Complete receiving request from Gemini, add styling and animation, render markdown syntax

--- public/newScript.js
+++ public/newScript.js
@@ -1,28 +1,36 @@
-function typeEffect(message, text) {
+const typingText = 'Assistant is typing...';
+
+$(document).ready(function() {
+    typeEffect(true);
+});
+
+const scrollToChatHistoryBottom = () => {
+    const chatHistoryContainer = $('.chat-history-container');
+    $(chatHistoryContainer).animate({ scrollTop: $(chatHistoryContainer)[0].scrollHeight }, "slow");
+}
+
+const typeEffect = (firstMessage = false) => {
+    const message = $('.message.typing');
+    message.empty();
     let index = 0;
-    if (index < text.length) {
+    if (index < typingText.length) {
         let interval = setInterval(() => {
-            message.append(text[index]);
+            message.append(typingText[index]);
             index++;
-            if (index === text.length) {
+            if (index === typingText.length) {
                 clearInterval(interval)
-                setTimeout(() => {
-                    message.removeClass('typing')
-                    message.html('Hello! How can I help you today?')
-                }, 1000)
+                if (firstMessage) {
+                    setTimeout(() => {
+                        message.removeClass('typing')
+                        message.html('Hello! How can I help you today?')
+                    }, 1000)
+                }
             }
         }, 50); // Adjust the speed here (in milliseconds)
     }
 }
 
-$(document).ready(function() {
-    let text = 'Assistant is typing...';
-    const message = $('.message.typing');
-    message.empty();
-    typeEffect(message, text);
-});
-
-$(document).on('submit', '#chat-form', function(event){
+$(document).on('submit', '#chat-form', async (event) =>{
     event.preventDefault();
     const newMessageInput = $('#newMessage');
     const message = newMessageInput.val();
@@ -31,6 +39,48 @@ $(document).on('submit', '#chat-form', function(event){
             <div class="message user-message">${message}</div>
             <div class="clearfix"></div>
         `);
+        scrollToChatHistoryBottom();
         newMessageInput.val('');
+        setTimeout(() => {
+            $('.chat-history-container').append(`
+                <div class="message system-message typing"></div>
+                <div class="clearfix"></div>
+            `);
+            scrollToChatHistoryBottom();
+            typeEffect();
+        }, 1000)
+        getGeminiResponseByQuestion(message);
     }
 });
+
+const getGeminiResponseByQuestion = async (question) => {
+    const response = await fetch('http://localhost:4000/ask/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question: question })
+    })
+
+    if(response.ok) {
+        console.log('yes');
+        const data = await response.json();
+        const formattedText = marked.parse(data.answer);
+        const message = $('.message.typing');
+        message.html(formattedText);
+        message.removeClass('typing');
+        scrollToChatHistoryBottom();
+    } else {
+        setTimeout(() => {
+            const message = $('.message.typing');
+            message.html('Sorry, I am unable to process your request at the moment. Please try again later.');
+            message.removeClass('typing');
+            $('#newMessage')
+                .attr('placeholder', 'Please try again later.')
+                .attr('disabled', 'disabled');
+            scrollToChatHistoryBottom();
+        }, 5000);
+    }
+}
+
+// if I don't have the health claim credentials, where should I email to?
